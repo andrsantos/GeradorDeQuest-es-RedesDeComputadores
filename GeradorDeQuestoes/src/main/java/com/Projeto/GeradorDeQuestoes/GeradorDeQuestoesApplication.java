2b332f9e068b8a513faf319e56db275a3bc07056
AndreySantos <santosandrey146@gmail.com>
2025-11-09 13:35:08 -0300

    Commit inicial - 09/11/2025

---
+++ GeradorDeQuestoes/src/main/java/com/Projeto/GeradorDeQuestoes/GeradorDeQuestoesApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto.GeradorDeQuestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeQuestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeQuestoesApplication.class, args);
+	}
+
+}
