780c7501f99933a8a64e21084f35eaabe1b82f77
AndreySantos <santosandrey146@gmail.com>
2025-11-11 13:28:49 -0300

    Adicionando página inicial à interface - 11/11/2025

--- gerador-de-questoes-ui/src/app/app.routes.ts
+++ gerador-de-questoes-ui/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { GeradorProva } from './pages/gerador-prova/gerador-prova';
-
+import { Dashboard } from './components/dashboard/dashboard';
 export const routes: Routes = [
-    {
-        path: '',
-        component: GeradorProva
-    }
+   {
+    path: '', 
+    component: Dashboard,
+    title: 'Início - Gerador de Provas'
+  },
+  {
+    
+    path: 'gerar-prova', 
+    component: GeradorProva,
+    title: 'Gerar Nova Prova'
+  }
 ];
--- gerador-de-questoes-ui/src/app/app.ts
+++ gerador-de-questoes-ui/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Sidebar } from './components/sidebar/sidebar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss' 
 })
 export class App {
   protected readonly title = signal('gerador-de-questoes-ui');
